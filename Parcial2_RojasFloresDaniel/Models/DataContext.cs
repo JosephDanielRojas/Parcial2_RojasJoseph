4167c138630ffbb521e39ba59195734e034dbbc0
Joseph Daniel Rojas Flores <a2019113821@estudiantes.upsa.edu.bo>
2021-04-24 10:58:59 -0400

    Se creo el controlador de modelo Friend

---
+++ Parcial2_RojasFloresDaniel/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2_RojasFloresDaniel.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Parcial2_RojasFloresDaniel.Models.Friend> Friends { get; set; }
+    }
+}
